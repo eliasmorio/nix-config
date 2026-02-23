2830ffc1b3937f9566fb2636c11c31ec757a3ca0
eliasmorio <eliasmorio@users.noreply.github.com>
2026-02-23 21:28:23 +0100

    fix: remove old import

--- hosts/thinkserver1/configuration.nix
+++ hosts/thinkserver1/configuration.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../../modules/linux/home-assistant.nix
     ];
   
   boot.loader.systemd-boot.enable = true;
