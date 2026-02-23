bc504baa5156b992371c182c956e53050ae447cc
eliasmorio <eliasmorio@users.noreply.github.com>
2026-02-23 21:50:13 +0100

    feat: add extraPackages for Home Assistant

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -3,6 +3,7 @@
   services.home-assistant = {
     enable = true;
     extraComponents = [ "default_config" "met" "esphome" "radio_browser" ];
+    extraPackages = python3Packages: [ python3Packages.pyatv ];
     config = {
       homeassistant = {
         name = "home assistant";
