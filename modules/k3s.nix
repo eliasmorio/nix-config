564f9b7942c73d6198e7c5350778f7299a5e6368
eliasmorio <eliasmorio@users.noreply.github.com>
2026-03-07 09:36:54 +0100

    feat: enhance k3s configuration with additional flags and system packages

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -1,12 +1,23 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   services.k3s = {
     enable = true;
     role = "server";
     extraFlags = toString [
-      "--disable traefik"
-      "--disable metrics-server"
-      "--write-kubeconfig-mode 644"
+      "--cluster-init"
+      "--disable=traefik"
+      "--disable=metrics-server"
+      "--disable=servicelb"
+      "--write-kubeconfig-mode=0644"
     ];
   };
+
+  environment.systemPackages = with pkgs; [
+    k3s
+    kubectl
+    fluxcd
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 6443 ];
+  networking.firewall.allowedUDPPorts = [ 8472 ];
 }
