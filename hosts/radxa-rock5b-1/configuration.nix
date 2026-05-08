1e934b51a77c2cc96b1958bd997a664704d13b30
eliasmorio <eliasmorio@users.noreply.github.com>
2026-05-08 15:53:36 +0200

    feat(rock5b-1): init nixos config, remove old one

--- hosts/radxa-rock5b-1/configuration.nix
+++ hosts/radxa-rock5b-1/configuration.nix
@@ -1,70 +1,107 @@
-# Radxa Rock 5B configuration
-{
-  lib,
-  pkgs,
-  ...
-}:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ../../modules/nixos/common.nix
-    ../../modules/nixos/locale-fr.nix
-    ../../modules/nixos/users/emorio.nix
-    ../../modules/nixos/ssh-server.nix
-    ../../modules/nixos/node-exporter.nix
-    # ../../modules/nixos/radxa-rock5b-kernel.nix
-    # ../../modules/k3s-single.nix
-  ];
-
-  # Use latest mainline kernel (matches working /etc/nixos config)
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Bootloader (kernel is configured via radxa-rock5b-kernel module)
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
-
-  # Networking with static IP
-  networking = {
-    hostName = "radxa-rock5b-1";
-    useDHCP = false;
-
-    interfaces.enP4p65s0.ipv4.addresses = [
-      {
-        address = "192.168.2.205";
-        prefixLength = 24;
-      }
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
     ];
 
-    defaultGateway = "192.168.2.254";
-    nameservers = [ "192.168.2.202" ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Host-specific: add networkmanager group
-  users.users.emorio.extraGroups = lib.mkAfter [ "networkmanager" ];
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.emorio = {
+    isNormalUser = true;
+    description = "emorio";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    openssh.authorizedKeys.keyFiles = [
+        ./github.keys
+    ];
+  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # Host-specific packages
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    home-manager
-    # Kernel module development
-    gnumake
-    gcc
-    binutils
-    pkg-config
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
-  # Enable NTP time synchronization (Rock 5B has no battery-backed RTC)
-  services.timesyncd.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  # Create /usr/lib/modules for systemd namespace sandboxing compatibility
-  systemd.tmpfiles.rules = [
-    "d /usr/lib/modules 0755 root root -"
-  ];
+  # List services that you want to enable:
 
-  system.stateVersion = "25.11";
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+
 }
+
