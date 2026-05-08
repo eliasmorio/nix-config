e56194ba54caa8a2aa6472eca3f62f10b878f9ca
eliasmorio <eliasmorio@users.noreply.github.com>
2026-05-08 16:29:34 +0200

    feat(radxa-rock5b): update NixOS configuration and add Home Manager setup - Enhance configuration with additional imports and static IP settings - Introduce Home Manager configuration for user management - Remove obsolete custom kernel module definitions

--- hosts/radxa-rock5b-1/configuration.nix
+++ hosts/radxa-rock5b-1/configuration.nix
@@ -1,36 +1,44 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+# radxa-rock5b-1 NixOS configuration
+{ lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/nixos/common.nix
+    ../../modules/nixos/users/emorio.nix
+    ../../modules/nixos/ssh-server.nix
+  ];
+
+  boot = {
+    # Bootloader
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    # Use latest kernel
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  # Networking with static IP
+  networking = {
+    hostName = "radxa-rock5b-1";
+    useDHCP = false;
+    networkmanager.enable = true;
+
+    interfaces.enP4p65s0.ipv4.addresses = [
+      {
+        address = "192.168.2.205";
+        prefixLength = 24;
+      }
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    defaultGateway = "192.168.2.254";
+    nameservers = [ "192.168.2.202" ];
+  };
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Host-specific: add networkmanager group
+  users.users.emorio.extraGroups = lib.mkAfter [ "networkmanager" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
+  # Locale
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -45,63 +53,16 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Configure keymap in X11
+  # Keyboard layout
   services.xserver.xkb = {
     layout = "fr";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "fr";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.emorio = {
-    isNormalUser = true;
-    description = "emorio";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-    openssh.authorizedKeys.keyFiles = [
-        ./github.keys
-    ];
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-
+  system.stateVersion = "25.11";
 }
-
